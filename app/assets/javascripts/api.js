6757f726e54b82b2191ad363730ffe975fae0595
Miriam Moser <mirjoy.moser@gmail.com>
2015-06-20 12:35:36 -0600

    edited click events to work with new html mark up

--- app/assets/javascripts/api.js
+++ app/assets/javascripts/api.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
   $('.entity').on('click', function(){
     var entity = $(this).find("#entity-title").text()
-    $('#home_news_query').val(entity);
+    $('#news-query').val(entity);
     $('html, body').animate({ scrollTop: 0 }, 'fast');
   })
 
@@ -27,5 +27,4 @@ $(document).ready(function(){
     var query = $('#news-query').val();
     sendEntityToController(query, entity);
   });
-
 });
